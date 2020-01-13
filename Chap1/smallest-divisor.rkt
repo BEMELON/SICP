f24e70ee10b95bc7016599d5bd80cec22c3db48f
BEMELON <hgdkim2@naver.com>
2020-01-13 19:39:29 +0900

    Update Smallest-divisor related Files

--- Chap1/smallest-divisor.rkt
+++ Chap1/smallest-divisor.rkt
@@ -1,41 +1,49 @@
 #lang sicp
-(#%provide smallest-divisor)
-(#%provide prime?)
-(#%provide fast-find-divisor)
-(#%provide fast-prime?)
+(#%provide search-for-primes smallest-divisor
+           square divides? start-prime-test)
+; (define (smallest-divisor n term)
+; (define (search-for-primes n limit  prime-method)
 
-(define (smallest-divisor n)
-  (find-divisor n 2))
-
-(define (fast-smallest-divisor n)
-  (fast-find-divisor n 2))
-
-
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
-
-(define (next divisor)
-  (if (= divisor 2)
-      (+ 1 divisor)
-      (+ 2 divisor)))
-     
-;; ex1.23
-(define (fast-find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (next test-divisor )))))
-
+;; Common Module
 (define (divides? a b)
   (= (remainder b a) 0))
 
 (define (square x)
   (* x x))
 
+(define (find-divisor n test-divisor term)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (term test-divisor) term))))
 
-(define (fast-prime? n)
-  (= n (fast-smallest-divisor n)))
+(define (smallest-divisor n term)
+    (find-divisor n 2 term))
 
-(define (prime? n)
-  (= n (smallest-divisor n)))
+;; Time Checking method
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+;; General method for Prime check
+(define (search-for-primes n limit  prime-method)
+  (cond ((> n limit) (display "\nDONE\n"))
+        (else
+         (begin (timed-prime-test n  prime-method)
+                (search-for-primes (+ 1 n) limit prime-method)))))
+
+(define (timed-prime-test n  prime-method)
+  (start-prime-test n (runtime)  prime-method))
+
+;; Display if N is Prime nubber
+(define (start-prime-test n start-time prime-method)
+      (if (prime-method n)
+          (begin
+            (newline)
+            (display n)
+            (report-prime (- (runtime) start-time)))))
+
+
+
+
+
+
