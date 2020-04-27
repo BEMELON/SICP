f0210891398a06afd90cbcefa3e54db6620de724
BEMELON <hgdkim2@naver.com>
2020-04-27 15:52:23 +0900

    Upload Exercise 2.36

---
+++ Chap2/ex2.36.rkt
@@ -0,0 +1,20 @@
+#lang sicp
+(define (accumulate op init seqs)
+  (if (null? seqs)
+      init
+      (op (car seqs)
+          (accumulate op init (cdr seqs)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map
+                                 (lambda (x) (car x))
+                                 seqs))
+            (accumulate-n op init (map
+                                   (lambda (x) (cdr x))
+                                   seqs)))))
+
+(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+(accumulate-n + 0 s)
+                                    
