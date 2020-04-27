0a58fdfe924e37c7390417d8d52367921aa4d6e1
BEMELON <hgdkim2@naver.com>
2020-04-27 16:03:34 +0900

    Upload Exercise 2.38

---
+++ Chap2/ex2.38.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+(define (fold-left op initial seq)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial seq))
+
+(define (fold-right op initial seq)
+  (if (null? seq)
+      initial
+      (op (car seq)
+          (fold-right op initial (cdr seq)))))
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(fold-right list nil (list 1 2 3))
+(fold-left list nil (list 1 2 3))
