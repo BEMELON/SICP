263d7d8483abf22068c9b6301d5e1d98be22f664
BEMELON <hgdkim2@naver.com>
2020-06-11 05:39:41 +0000

    Upload Execise 2.42

---
+++ Chap2/ex2.42.rkt
@@ -0,0 +1,55 @@
+#lang sicp
+
+(define (filter proc seq)
+    (cond ((null? seq) nil)
+          ((proc (car seq)) (cons (car seq) (filter proc (cdr seq))))
+          (else (filter proc (cdr seq)))))
+            
+(define (accumulate op init seq)
+    (if (null? seq)
+        init
+        (op (car seq)
+            (accumulate op init (cdr seq)))))
+
+(define (flatmap proc seq)
+    (accumulate append nil (map proc seq)))
+    
+(define (map proc seq)
+    (if (null? seq)
+        nil
+        (cons (proc (car seq)) (map proc (cdr seq)))))
+
+(define (enumerate-interval n last)
+    (if (> n last)
+        nil
+        (cons n (enumerate-interval (+ 1 n) last))))
+           
+        
+        
+(define empty-board nil)
+
+(define (safe? k positions)
+    (display "[DBG] <safe?> positions |=> ")
+    (display positions) (newline)
+    #true)
+    
+(define (adjoin-position new-row k rest-of-queens)
+    (append rest-of-queens (cons new-row k)))
+    
+(define (queens board-size)
+    (define (queen-cols k)
+        (if (= k 0)
+            (list empty-board)
+            (filter
+                (lambda (positions) (safe? k positions))
+                (flatmap
+                    (lambda (rest-of-queens)
+                        (map 
+                            (lambda (new-row) (adjoin-position new-row k rest-of-queens))                          
+                            (enumerate-interval 1 board-size)))
+                    (queen-cols (- k 1))))))
+    (queen-cols board-size))
+
+(queens 7)
+
+                
