bfb6ed44f8249fc08e89332b8191e6cb74fb3c1e
BEMELON <hgdkim2@naver.com>
2020-06-20 11:52:59 +0000

    Update Exercise 2.54 (recursive eq?)

--- Chap2/ex2.54.rkt
+++ Chap2/ex2.54.rkt
@@ -1,15 +1,31 @@
 #lang sicp
   
-(define (precise-equal? x y)
-    (define (flatmap seq)
-        (cond   ((null? seq) '())
-                ((pair? (car seq)) (append (flatmap (car seq)) (flatmap (cdr seq))))
-                (else (append (list (car seq)) (flatmap (cdr seq))))))
-            
-    (let ((x (flatmap x))
-          (y (flatmap y)))
-          (equal? x y)))
+     
+(define (equal? x y) 
+    ;(display "[DBG] <equal?> x |=> ") (display x) (newline)
+    ;(display "[DBG] <equal?> y |=> ") (display y) (newline)
+    (if (and (null? x) (null? y)) 
+        #true
+          (begin
+              (let ((token_x (car x))
+                    (token_y (car y)))
+                
+                  (cond   ((and (symbol? token_x) (symbol? token_y)) 
+                                (and (eq? token_x token_y)
+                                     (equal? (cdr x) (cdr y))))                                                          
+                          ((and (not (pair? token_x)) (pair? token_y))  
+                                (and (equal? (list token_x) (list (car token_y)))
+                                     (equal? (list (cadr x)) (list (cadr token_y)))
+                                     (equal? (list (cdr (cdr x))) (list (cdr y)))))
+                                     
+                          ((and (pair? token_x) (not (pair? token_y)))
+                                (and (equal? (list (car token_x)) (list token_y))
+                                     (equal? (list (cadr token_x)) (cdr y))
+                                     (equal? (list (cdr x)) (list (cdr (cdr y))))))
+                          (else 
+                                (and (equal? (list (car token_x)) (list (car token_y))))))))))
 
-(precise-equal? '(this is a list) '(this (is a) list))
-      
-      
+
+
+(equal? '(this is a list) '(this is a list))      
+(equal? '(this is a list) '(this (is a) liste))
