6ed7e0012fd60bbed088f1e87933f4627b4dc75b
BEMELON <hgdkim2@naver.com>
2020-07-23 06:38:03 +0000

    Upadte GET/PUT Hastable

--- util/util.rkt
+++ util/util.rkt
@@ -1,7 +1,22 @@
 #lang sicp
-(#%require (only racket/base current-inexact-milliseconds))
+(#%require (only racket/base current-inexact-milliseconds)
+           (only racket 
+                 make-hash
+                 hash-set!
+                 hash-ref!))
 
-(#%provide runtime)
+
+(#%provide runtime put get)
+
+; Provide Runtime 
 (define (runtime) (current-inexact-milliseconds))
 
 
+; PUT GET for Hash Table
+(define *op-table* (make-hash))
+
+(define (put op type proc)
+  (hash-set! *op-table* (list op type) proc))
+
+(define (get op type)
+  (hash-ref! *op-table* (list op type) '()))
