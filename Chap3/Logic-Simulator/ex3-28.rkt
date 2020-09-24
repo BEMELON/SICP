233f20e47e436c0fec874623799a8ca54c2a5600
BEMELON <hgdkim2@naver.com>
2020-09-24 10:58:47 +0000

    Upload Exercise 3.28

---
+++ Chap3/Logic-Simulator/ex3-28.rkt
@@ -0,0 +1,16 @@
+#lang sicp
+(#%require "tools.rkt")
+
+; Exercise 3.28
+; Define an or-gate as a primitive function box.
+; Your or-gate constructor should be similar to andgate.
+
+(define (or-gate a1 a2 output)
+    (define (or-action-procedure)
+        (let ((computed (logical-or (get-signal a1) (get-signal a2))))
+             (after-delay
+                 or-gate-delay
+                 (lambda () (set!-signal! output computed)))))
+    (add-action! a1 or-action-procedure)
+    (add-action! a2 or-action-procedure)
+    'ok)
